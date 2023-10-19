631663728384514b7bc01a6887053bb480c602b2
Guilherme H. Jardim <>
2023-10-19 03:56:58 -0300

    change plotting style

--- core.py
+++ core.py
@@ -38,12 +38,11 @@ def visualize_graph(G, pos=None, colors=None):
     plt.figure(figsize=(10, 8))
 
     if colors is not None:
-        nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=20, font_size=8, width=0.5, node_color=colors, cmap=plt.cm.viridis)
+        nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=20, font_size=8, width=0.1,
+                node_color=colors, cmap=plt.cm.viridis)
     else:
-        nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=20, font_size=8, width=0.5)
+        nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=20, font_size=8, width=0.1)
 
-    labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=8)
     plt.show()
 
 def perform_clustering(G, adjacency_matrix, num_clusters=10):
