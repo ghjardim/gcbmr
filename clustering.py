a07c21db6661a9c5a1557d7290ae1cf295c71534
Lucas Quintas Honorato <lucasquintas@hotmail.com>
2023-10-29 19:18:50 -0300

    refactor: move functions to clustering.py

--- clustering.py
+++ clustering.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np
+import plot_graph
 
 def get_movie_cluster(movie_title, movie_indices, G):
     movie_index = movie_indices.get(movie_title)
@@ -79,3 +80,26 @@ def include_target_movie_in_cluster_subgraph(G, subgraph, target_movie):
     new_subgraph.add_edges_from(target_movie_edges)
 
     return new_subgraph
+
+def create_cluster_subgraph(G, cluster_number):
+    cluster_indices = [node for node, data in G.nodes(data=True) if data.get('cluster') == cluster_number]
+
+    if not cluster_indices:
+        print(f"No nodes found in cluster {cluster_number}. Returning an empty subgraph.")
+        return nx.Graph()
+
+    # Create a subgraph with nodes and edges only from the specified cluster
+    subgraph = G.subgraph(cluster_indices)
+
+    return subgraph
+
+def perform_clustering(G, num_clusters=10):
+    adjacency_matrix = nx.adjacency_matrix(G).toarray()
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42, n_init=20)
+    cluster_labels = kmeans.fit_predict(adjacency_matrix)
+
+    for node, label in enumerate(cluster_labels):
+        G.nodes[node]['cluster'] = label
+
+    colors = [G.nodes[node]['cluster'] for node in G.nodes]
+    plot_graph.visualize_graph(G, colors=colors)
